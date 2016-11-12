759fbb8fbde88ba80809cdd352e5494ff6360ffe
ken5scal <kengoscal@gmail.com>
2016-11-12 11:25:05 +0900

    Add test for KyoriWoHyoji Method

--- ekikan_test.go
+++ ekikan_test.go
@@ -31,3 +31,27 @@ func Test_GetEkikanKyori_Between_Directy_Not_Connected(t *testing.T) {
 		t.Error("got non infinite number")
 	}
 }
+
+func Test_KyoriWoHyoji(t *testing.T) {
+	expected := "茗荷谷駅と新大塚駅までは1.2kmです"
+	actual := KyoriWoHyoji("myogadani", "shinotsuka")
+	if actual != expected {
+		t.Errorf("got %v instead of %v", actual, expected)
+	}
+}
+
+func Test_KyoriWoHyoji_Not_Connected(t *testing.T) {
+	expected := "茗荷谷駅と千駄木駅はつながっていません"
+	actual := KyoriWoHyoji("myogadani", "sendagi")
+	if actual != expected {
+		t.Errorf("got %v instead of %v", actual, expected)
+	}
+}
+
+func Test_KyoriWoHyoji_DoNot_Exist(t *testing.T) {
+	expected := "I dont existという駅は存在しません"
+	actual := KyoriWoHyoji("myogadani", "I dont exist")
+	if actual != expected {
+		t.Errorf("got %v instead of %v", actual, expected)
+	}
+}
