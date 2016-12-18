4fabc18f3958c1ab4256e82c3aee5cbf22f1e4a7
ken5scal <kengoscal@gmail.com>
2016-12-18 13:14:03 +0900

    Add build

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
 func main() {
+	kiten := "shibuya"
+	syuten := "mitsukoshimae"
+
+	ekimei_list := &GlobalEkimei{EkimeiList: Ekimei_List}
+	ekimei_list.Seiretsu()
+	kiten = ekimei_list.RomajiToKanji(kiten)
+	syuten = ekimei_list.RomajiToKanji(syuten)
 
 }
